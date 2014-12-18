87caeab62271d6764b9a41c9f7a98ea5c00a1bf2
Eric T Dawson <eric.t.dawson@gmail.com>
2014-12-18 11:46:43 -0600

    Changed to sh script

--- platypus.sh
+++ platypus.sh
@@ -1,22 +1,69 @@
 ## Set up the bin directory
 tar xzf bin.tgz
-cd bin
+cd ./bin
+./simlink.sh
+cd ..
+PATH=$PATH:`pwd`/bin
 
+## Inputs: queryBAM,
+##         fasta file OR flat tgz/tar.gz file with fa and fai
+QBAM=$bamFiles
+QBAMIND=$bamInd
+extractRef.sh ${refFile}
+REFERENCE_F=$(basename $(find ${TARGET}/ -name "*.${EXTENSION}" -print0))
+REFERENCE_F="${TARGET}/$REFERENCE_F"
 
-## Split files by chromosome region using GTK, UNLESS
-## a specific region is passed in as an argument
-if [ -d ${regions} ]
+WHOLEGENOME=0
+if [ -z `echo ${regions} | sed "s/ //g"` ]
+    then REGIONS=""; WHOLEGENOME=1
+else
+    REGIONS="--regions ${regions}"
+fi
+
+if [ -z `echo ${source} | sed "s/ //g"` ]
+    then SOURCE=""
+else
+    SOURCE="--source ${source}"
+fi
 
 ## Take in and assemble argument string
-ARGS= ${output} ${refFile} ${bamFiles} ${regions} ${assemble}
-ARGS+= ${source} ${nCPU} ${logFileName} ${bufferSize} ${minReads}
+ARGS= ${output} ${REFERENCE_F} ${QBAM} ${REGIONS} ${assemble}
+ARGS+= ${SOURCE} ${nCPU} ${logFileName} ${bufferSize} ${minReads}
 ARGS+= ${maxReads} ${maxVariants} ${verbosity} ${minPosterior}
 ARGS+= ${maxSize} ${minFlank}
-## Unpack reference if necessary
 
-## Handling paired-end data
 
+rm -rf paramlist.txt
 ## Run Platypus on the input files
-python Platypus.py callVariants ${ARGS}
+if [[ "$WHOLEGENOME" == 0]];
+    ##Run platypus on the specified region
+    then python Platypus.py callVariants ${ARGS}
+else
+    ## get BAM header line and find genomic length
+    seqID=`samtools view -H ${QBAM} | grep -o SQ:[A-Za-z0-9]`
+    genomeLen=`samtools view -H ${QBAM} | grep -o "LN:[0-9]*" | grep -o "[0-9]*"`
+    ## Divide the genome up into portions that give each core 1,000,000bp
+    numWorkers=((${genomeLen}/${IPLANT_CORES_REQUESTED}))
+    remainder=((${genomeLen}%${IPLANT_CORES_REQUESTED}))
+    for i in `seq 1 numWorkers`
+    do
+        REGIONS=$seqID:$((`expr $i - 1` * $numWorkers))-$(($i * $numWorkers))
+        echo "python Platypus.py callVariants ${output} ${REFERENCE_F} ${QBAM} ${REGIONS} ${assemble} ${SOURCE} ${nCPU} ${logFileName} ${bufferSize} ${minReads} ${maxReads} ${maxVariants} ${verbosity} ${minPosterior} ${maxSize} ${minFlank}" >> paramlist.txt
+    done
+    REGIONS=$seqID:$(($i * $numWorkers))-$(($i * $numWorkers + $remainder))
+    echo "python Platypus.py callVariants " >> paramlist.txt
 
+    echo "Launcher...."
+    date
+    export TACC_LAUNCHER_SCHED=dynamic
+    EXECUTABLE=$TACC_LAUNCHER_DIR/init_launcher
+    $TACC_LAUNCHER_DIR/paramrun $EXECUTABLE paramlist.txt
+    date
+    echo "..Done"
+
+fi
 ## Clean up the inputs and remove the bin directory
+for i in ${TARGET} .launcher $QBAM $QBAMIND bin paramlist.txt
+do
+    rm -rf $i
+done
